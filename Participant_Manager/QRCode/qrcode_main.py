50ed24e540613adf8b5d33bcc2b462da0a46d3ec
MohammadAmin Alamalhoda <ma.alamalhoda@gmail.com>
2022-07-07 00:13:15 +0430

    prints are removed

--- Participant_Manager/QRCode/qrcode_main.py
+++ Participant_Manager/QRCode/qrcode_main.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 class QRCode():
     def __init__(self, data):
-        self.data = data['info']
+        self.qrcode_text = data['name'] + '\n' + data['info'] + '\n id=' + data['id']
         self.qr = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -19,19 +19,11 @@ class QRCode():
         self.make_image()
 
     def make_image(self):
-        print('fffff')
-        self.qr.add_data(self.data)
+        self.qr.add_data(self.qrcode_text)
         self.qr.make(fit=True)
         img = self.qr.make_image(fill_color="black", back_color="white")
         img.show()
-        img.save("qrcode.png")
         img.close()
-
- 
-    
-        
-        
-        
         # save the QR code generated
 
 
@@ -39,10 +31,7 @@ def symposium_img():
     logo = Image.open("assets/images/logo_symp.png")
     return logo
 
-# __main__ runs if this file is run directly from the command line and not
-# imported as a module.
 if __name__ == "__main__":
     data = {'name':'MohammadAmin Alamalhoda', 'info':'1 lunch', 'id':'123456789'}
-    print('fffff')
     qr_code_instance = QRCode(data)
     
