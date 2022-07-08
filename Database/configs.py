7e01626aa5391412886e6bc0081ea4654a080035
MohammadAmin Alamalhoda <ma.alamalhoda@gmail.com>
2022-07-08 22:20:52 +0430

    db class is made - api modified to accept POSˇ json

---
+++ Database/configs.py
@@ -0,0 +1,16 @@
+import operators
+
+class database_configs():
+    def __init__(self) -> None:
+        self.dbName = '../Database/db_main.db'
+        self.make_conn()
+        self.sql_create_table_users = """ CREATE TABLE IF NOT EXISTS users (
+                                        snsID text NOT NULL,
+                                        lunchs inter,
+                                        snacks integer,
+                                        id integer primary key
+                                    ); """
+        operators.create_table(self.conn, self.sql_create_table_users)
+
+    def make_conn(self):
+        self.conn = operators.create_connection(self.dbName)
--- PanelAPI/loginAPI_main.py
+++ PanelAPI/loginAPI_main.py
@@ -1,11 +1,27 @@
-from flask import Flask
+import flask
+from flask import Flask, jsonify
+import sys
+sys.path.append('../Database')
+
+import configs
 
 app = Flask(__name__)
-@app.route('/Panel', methods=['GET', 'POST'])
-def welcome():
-    return jsonify({'name':'Alam',
-                    'address':'In front of the computer'})
+
+@app.route('/panel', methods=['GET', 'POST'])
+def panel():
+    if flask.request.method == "POST":
+        data = flask.request.get_json()
+        print(data)
+        snsID = data['snsID']
+        cursor = db.conn.cursor()
+        cursor.execute('SELECT * FROM users WHERE snsID = ?', (snsID,))
+        user_info = cursor.fetchall()
+        return jsonify({'snsID':snsID})
+    else:
+        return jsonify({'Error':'500'})
+
 
 
 if __name__ == '__main__':
+    db = configs.database_configs()
     app.run(host='0.0.0.0', port=105)
