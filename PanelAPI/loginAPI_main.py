fd7c577714741968f514ba9c824bc004356b611e
MohammadAmin Alamalhoda <ma.alamalhoda@gmail.com>
2022-07-09 11:37:03 +0430

    qr code is made when calling panel from API

--- PanelAPI/loginAPI_main.py
+++ PanelAPI/loginAPI_main.py
@@ -1,9 +1,12 @@
 import flask
 from flask import Flask, jsonify
 import sys
-sys.path.append('../Database')
+sys.path.append('Database')
+sys.path.append('QRCode')
 
 import configs
+import qrcode_configs
+
 
 app = Flask(__name__)
 
@@ -18,15 +21,17 @@ def panel():
         snacks = 2
         cursor = db.conn.cursor()
         cursor.execute('SELECT * FROM users WHERE snsID = ?', (snsID,))
-        user_info = cursor.fetchall()
+        user_info = cursor.fetchone()
         if len(user_info)==0:
             cursor.execute('INSERT INTO users (snsID, name, email, lunchs, snacks) VALUES (?, ?, ?, ?, ?)',
                              (snsID, name, email, lunchs, snacks))
             db.conn.commit()
-        cursor.execute('SELECT * FROM users WHERE snsID = ?', (snsID,))
-        user_info = cursor.fetchall()
-        print(user_info)
-        return flask.render_template('panel.html', user_info=user_info[0])
+            cursor.execute('SELECT * FROM users WHERE snsID = ?', (snsID,))
+            user_info = cursor.fetchone()
+        qr_image = qr.make_image(user_info)
+        qr_image.show()
+        qr_image.close()
+        return flask.render_template('panel.html', user_info=user_info)
     else:
         return jsonify({'status':'500 Error'})
 
@@ -34,4 +39,5 @@ def panel():
 
 if __name__ == '__main__':
     db = configs.database_configs()
+    qr = qrcode_configs.QRCode()
     app.run(host='0.0.0.0', port=105)
